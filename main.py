9332ac1d78e9b15325ae27cb1c8c69fc8f8c33b2
Byron Velasco <andy-velasco10@hotmail.com>
2025-05-31 11:17:35 -0500

    first commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import joblib
 import numpy as np
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 model = joblib.load("RF_model.pkl")
